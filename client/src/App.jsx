552b08aa1c2352ebcfe56a0abfa3eced0e001909
nithyaganesh-dev <nithyaganesh.developer@gmail.com>
2025-07-11 11:32:02 +0530

    Client First Commit

---
+++ client/src/App.jsx
@@ -0,0 +1,36 @@
+import { Routes, Route, useLocation } from "react-router-dom"
+import { Toaster } from "react-hot-toast"
+
+import Navbar from "./components/Navbar"
+import Footer from "./components/Footer"
+
+import Home from "./pages/Home"
+import Movies from "./pages/Movies"
+import MovieDetails from "./pages/MovieDetails"
+import SetLayout from "./pages/SetLayout"
+import MyBookings from "./pages/MyBookings"
+import Favorite from "./pages/Favorite"
+
+
+const App = () => {
+
+  const isAdminRoute = useLocation().pathname.startsWith("/admin")
+
+  return (
+    <>
+      <Toaster />
+      {!isAdminRoute && <Navbar />}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/movies/:id" element={<MovieDetails />} />
+        <Route path="/movies/:id/:date" element={<SetLayout />} />
+        <Route path="/my-bookings" element={<MyBookings />} />
+        <Route path="/favorite" element={<Favorite />} />
+      </Routes>
+      {!isAdminRoute && <Footer />}
+    </>
+  )
+}
+
+export default App
---
+++ client/src/pages/SetLayout.jsx
@@ -0,0 +1,10 @@
+
+const SetLayout = () => {
+  return (
+    <div>
+      <h1>Set Layout</h1>
+    </div>
+  )
+}
+
+export default SetLayout
